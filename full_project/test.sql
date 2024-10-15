a0b53fb80a4edccf409eaa5165b87c56984d38bc
belmaayo <boubkerelmaayouf@gmail.com>
2024-10-15 16:40:51 +0100

    all the files

---
+++ full_project/test.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE Forum;
+USE Forum;
+
+CREATE TABLE Users (
+    user_id INTEGER PRIMARY KEY, 
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    email TEXT UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE PostCategories (
+    category_id INTEGER PRIMARY KEY, 
+    category_name TEXT UNIQUE NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Posts (
+    post_id INTEGER PRIMARY KEY, 
+    user_id INTEGER,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL,
+    category_id INTEGER,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (category_id) REFERENCES PostCategories(category_id)
+);
+
+CREATE TABLE Reactions (
+    reaction_id INTEGER PRIMARY KEY, 
+    post_id INTEGER,
+    user_id INTEGER,
+    reaction_type TEXT NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (post_id) REFERENCES Posts(post_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+INSERT INTO posts (title, content, user_id) VALUES ('kaka', 'dada',2 );
+ALTER TABLE Users ADD COLUMN fullname TEXT;
